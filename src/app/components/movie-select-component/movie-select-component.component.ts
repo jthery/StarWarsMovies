76468788ae31b1c1c8b069308e16f3a01aab44d6
jthery <j.thery2311@outlook.fr>
2019-11-18 22:39:37 +0100

    hide show button film ok

--- src/app/components/movie-select-component/movie-select-component.component.ts
+++ src/app/components/movie-select-component/movie-select-component.component.ts
@@ -8,26 +8,28 @@ import { Film } from '../../models/film.model';
   templateUrl: './movie-select-component.component.html',
   styleUrls: ['./movie-select-component.component.css']
 })
+
 export class MovieSelectComponentComponent implements OnInit {
-  public show:boolean = false;
-  public buttonName:any = 'Show';
+
   selectedFilm: string;
   films$: Observable<Film[]>;
+  public film;
+  public show:boolean = false;
+  public buttonName:any = 'Show';
 
   constructor(private filmService: FilmService) { }
 
   ngOnInit() {
     this.films$ = this.filmService.getFilms();
-    console.log(this.films$, 'herehere')
   }
 
   toggle() {
     this.show = !this.show;
-
     if(this.show)  
       this.buttonName = "Hide";
     else
       this.buttonName = "Show";
+      this.film = this.selectedFilm;
   }
 
 }
